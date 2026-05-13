379b1092247860fa9f37d9c7a52801e7b9367df6
vitalii.shveider <vshveyder@gmail.com>
2026-05-13 19:46:38 +0300

    fixes

--- js/core/EventBus.js
+++ js/core/EventBus.js
@@ -9,14 +9,6 @@ export class EventBus {
         this._listeners.get(event).push({ fn, ctx });
     }
 
-    once(event, fn, ctx = null) {
-        const wrapper = (...args) => {
-            fn.apply(ctx, args);
-            this.off(event, wrapper);
-        };
-        this.on(event, wrapper);
-    }
-
     off(event, fn) {
         if (!this._listeners.has(event)) return;
         const list = this._listeners.get(event);
--- js/core/PluginManager.js
+++ js/core/PluginManager.js
@@ -11,21 +11,6 @@ export class PluginManager {
         return this;
     }
 
-    unregister(name) {
-        const idx = this._plugins.findIndex(p => p.name === name);
-        if (idx !== -1) {
-            const plugin = this._plugins[idx];
-            if (plugin.destroy) plugin.destroy();
-            this._plugins.splice(idx, 1);
-        }
-    }
-
-    update(dt) {
-        for (const plugin of this._plugins) {
-            if (plugin.update) plugin.update(dt);
-        }
-    }
-
     destroy() {
         for (const plugin of this._plugins) {
             if (plugin.destroy) plugin.destroy();
